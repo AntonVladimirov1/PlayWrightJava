7ac94f2e12e6a4f59af34fd3da0394d6e0b5211b
Antonio_Git <l.a.v7232@gmail.com>
2024-12-24 14:12:12 -0600

    Day1 evening (extends BasePage)

--- src/test/java/com/uhaul/test/Hooks/BaseTestPage.java
+++ src/test/java/com/uhaul/test/Hooks/BaseTestPage.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-public class BasePage {
+public class BaseTestPage {
 
     static protected Browser browser;
     static protected Playwright playwright;
--- src/test/java/com/uhaul/test/day_01/Locators/PlayWright_Locators.java
+++ src/test/java/com/uhaul/test/day_01/Locators/PlayWright_Locators.java
@@ -1,15 +1,11 @@
 package com.uhaul.test.day_01.Locators;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.options.AriaRole;
-import com.uhaul.test.Hooks.BasePage;
-import org.junit.jupiter.api.BeforeAll;
+import com.uhaul.test.Hooks.BaseTestPage;
 import org.junit.jupiter.api.Test;
 
-public class PlayWright_Locators extends BasePage {
+public class PlayWright_Locators extends BaseTestPage {
 
 
     @Test
@@ -23,12 +19,11 @@ public class PlayWright_Locators extends BasePage {
         //page.querySelector("#ID").fill("incorrectPassword");
         page.getByRole(AriaRole.BUTTON,new Page.GetByRoleOptions().setName("Sign In")).click();
         page.waitForTimeout(3000);
+
         System.out.println(page.getByRole(AriaRole.ALERT).isVisible());
         System.out.println(page.getByRole(AriaRole.ALERT).textContent());
 
-
         page.waitForTimeout(3000);
-
 
 
     }
